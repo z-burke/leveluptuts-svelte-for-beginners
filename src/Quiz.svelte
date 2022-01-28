96ba9e4540b540577b5400f111f35340b38e6ab7
Zachary Burke <zachary.burke@radixeng.com>
2022-01-28 08:12:59 -0600

    07 Conditionals & Loops

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,6 +1,7 @@
 <script>
     let result = "";
     let correctAnswer = "b";
+    let answers = ["a", "b", "c", "d"];
 
     function pickAnswer(answer) {
         if (answer == correctAnswer) {
@@ -11,9 +12,14 @@
 </script>
 
 <div>
-    <h4>{result}</h4>
-    <button on:click={() => pickAnswer('a')}>Answer A</button>
-    <button on:click={() => pickAnswer('b')}>Answer B</button>
-    <button on:click={() => pickAnswer('c')}>Answer C</button>
-    <button on:click={() => pickAnswer('d')}>Answer D</button>
+    {#if result}
+        <h4>{result}</h4>
+    {:else}
+        <h5>Please pick an answer</h5>
+    {/if}
+    {#each answers as answer}
+        <button on:click={() => pickAnswer(answer)}>
+            Answer {answer.toUpperCase()}
+        </button>
+    {/each}
 </div>
