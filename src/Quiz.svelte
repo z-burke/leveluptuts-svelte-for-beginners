249686186314071d4a685f4ebd9466eb33b73d8d
Zachary Burke <zachary.burke@radixeng.com>
2022-01-28 09:07:35 -0600

    09 Hitting an API

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,13 +1,22 @@
 <script>
-    let result = "";
+    let result;
     let correctAnswer = "b";
     let answers = ["a", "b", "c", "d"];
+    let quiz = getQuiz();
 
     function pickAnswer(answer) {
         if (answer == correctAnswer) {
             return (result = "Correct!");
         }
         result = "OOPS";
+    }
+
+    async function getQuiz() {
+        const res = await fetch(
+            "https://opentdb.com/api.php?amount=10&category=12&type=multiple"
+        );
+        const quiz = await res.json();
+        debugger;
     }
 </script>
 
@@ -18,11 +27,16 @@
 </style>
 
 <div>
+    <button on:click={getQuiz}>Get Questions</button>
     {#if result}
         <h4>{result}</h4>
     {:else}
         <h5>Please pick an answer</h5>
     {/if}
+
+    <h3>{quiz.results[0].question}</h3>
+
+
     {#each answers as answer}
         <button on:click={() => pickAnswer(answer)}>
             Answer {answer.toUpperCase()}
