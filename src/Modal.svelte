89001be22fc4ea3b58ac6d94f8b4889bdb587be6
Zachary Burke <zachary.burke@radixeng.com>
2022-01-29 21:24:09 -0600

    CSS & Post CSS Processing

--- src/Modal.svelte
+++ src/Modal.svelte
@@ -5,7 +5,7 @@
     const dispatch = createEventDispatcher();
 </script>
 
-<style>
+<style lang="scss">
     .modal-bg {
         position: fixed;
         top: 0;
@@ -13,11 +13,11 @@
         right: 0;
         bottom:0;
         background: rgba(0, 0, 0, 0.8);
-    }
-    .modal {
-        background: white;
-        padding: 20px;
-        border-radius: 15px;
+        .modal {
+            background: white;
+            padding: 20px;
+            border-radius: 15px;
+        }
     }
 </style>
 
